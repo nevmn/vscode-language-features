fc56e0af22fb621d6079e827168f186cc4edfc02
Maxim Neverov <nevmn21@gmail.com>
2021-07-22 19:37:24 +0600

    resolve issue with experimental compiler option

--- build.sh
+++ build.sh
@@ -2,14 +2,15 @@
 
 HOME=$(pwd)
 
+yarn add typescript@4.4.0-dev.20210607
+
 for server in "css" "html" "json"
 do
     folder="./vscode/extensions/$server-language-features/server"
     cd "$folder" && \
         yarn && \
-        yarn add -D typescript && \
         cd "$HOME" && \
-        tsc -p "$folder" --lib 'webworker' --outDir "./$server-language-features" && \
+        yarn tsc -p "$folder" --lib 'webworker' --outDir "./$server-language-features" && \
         cp "$folder/package.json" "./$server-language-features" && \
         sed -i '$s/}/,\n"bin":{"'"$server"'-languageserver":"node\/'"$server"'ServerMain.js"}\n}/' "./$server-language-features/package.json" && \
         sed -i '1s;^;#!/usr/bin/env node\n;' "$server-language-features/node/${server}ServerMain.js" && \
